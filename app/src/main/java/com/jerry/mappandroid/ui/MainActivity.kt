fd146495732bdd6c586af093cfded6b1a49c674d
leesy <leesy@rsupport.com>
2021-03-28 01:14:00 +0900

    현재 위치로 지도 이동 버튼 액션 연결

--- app/src/main/java/com/jerry/mappandroid/ui/MainActivity.kt
+++ app/src/main/java/com/jerry/mappandroid/ui/MainActivity.kt
@@ -1,28 +1,29 @@
 package com.jerry.mappandroid.ui
 
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Base64
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.jerry.mappandroid.R
 import com.jerry.mappandroid.databinding.ActivityMainBinding
-import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+
+    private val mainViewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
+
+    private lateinit var mapView : MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.testView.addView(MapView(this).apply {
-            setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true)
-
+        mapView = MapView(this)
+        binding.mainMapView.addView(mapView)
+        mainViewModel.currentMapPoint.observe(this, { mapPoint ->
+            mapView.setMapCenterPoint(mapPoint, true)
         })
     }
+
 }
--- app/src/main/java/com/jerry/mappandroid/ui/MainViewModel.kt
+++ app/src/main/java/com/jerry/mappandroid/ui/MainViewModel.kt
@@ -1,7 +1,27 @@
 package com.jerry.mappandroid.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import net.daum.mf.map.api.MapPoint
+
 /**
  * Created by jerry on 2021/03/20
  **/
-class MainViewModel {
+class MainViewModel : ViewModel(){
+    private val _currentMapPoint = MutableLiveData<MapPoint>()
+    val currentMapPoint : LiveData<MapPoint>
+    get() = _currentMapPoint
+
+    fun onClickCurrentLocation(){
+        // TODO check permission of LOCATION
+        getCurrentLocationMapPoint()
+    }
+
+    private fun getCurrentLocationMapPoint() : MapPoint {
+        //TODO get current location
+        return MapPoint.mapPointWithGeoCoord(0.0,0.0)
+    }
+
+
 }
